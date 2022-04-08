fa31011e8b1adb0c4d98346028649e18e6b579d3
Arend-Jan Kramer <arendjan@skrepr.com>
2022-04-08 11:55:33 +0200

    fix yml to yaml

--- src/DependencyInjection/SkreprAuthExtension.php
+++ src/DependencyInjection/SkreprAuthExtension.php
@@ -12,7 +12,7 @@ class SkreprAuthExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('routing.yml');
+        $loader->load('routing.yaml');
     }
 }
 
