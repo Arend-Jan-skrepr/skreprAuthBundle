80f19316ef4d20e0ddbd9d09e0a18145adebd914
Arend-Jan Kramer <arendjan@skrepr.com>
2022-04-08 11:28:39 +0200

    Change xml file loader to Yaml

--- src/DependencyInjection/SkreprAuthExtension.php
+++ src/DependencyInjection/SkreprAuthExtension.php
@@ -5,17 +5,14 @@ namespace Skrepr\SkreprAuthBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class SkreprAuthExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader(
-            $container,
-            new FileLocator(__DIR__.'/../Resources/config')
-        );
-        $loader->load('services.xml');
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yaml');
     }
 }
 
