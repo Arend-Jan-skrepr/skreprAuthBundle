7cbe4054796340c8aeb0c37e0fa173826553cbb3
Arend-Jan Kramer <arendjan@skrepr.com>
2022-04-08 11:16:06 +0200

    create develop branch

---
+++ src/DependencyInjection/SkreprAuthExtension.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Skrepr\SkreprAuthBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class SkreprAuthExtension extends Extension
+{
+
+}
