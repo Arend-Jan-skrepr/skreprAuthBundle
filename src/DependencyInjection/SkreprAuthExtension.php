8c1ada23494e4a0a286ade6e95120cec2a297b04
Arend-Jan Kramer <arendjan@skrepr.com>
2022-04-14 14:57:05 +0200

    add load extension

--- src/DependencyInjection/SkreprAuthExtension.php
+++ src/DependencyInjection/SkreprAuthExtension.php
@@ -6,6 +6,13 @@ use Symfony\Component\DependencyInjection\Extension\Extension;
 
 class SkreprAuthExtension extends Extension
 {
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = $this->getConfiguration($configs, $container);
+
+        return $configuration;
+    }
+
     public function getAlias()
     {
         return 'skrepr_auth';
